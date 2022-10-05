0dfabb0afaa994f3acc5d441a6fa2c620facbe98
kaolnwza <sinlapawit.kao@gmail.com>
2022-10-06 01:45:23 +0700

    Remove rude api

--- server/apisrv/controller/test.go
+++ server/apisrv/controller/test.go
@@ -2,15 +2,20 @@ package controller
 
 import (
 	"net/http"
+	"tomato-api/lib/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Controller(port string) {
 	r := gin.Default()
+
+	r.Use(config.CorsConfig())
+	r.SetTrustedProxies([]string{"*"})
+
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
-			"msg": "kuy rai krub",
+			"msg": "Hello UNIVERSE!!",
 		})
 	})
 	r.GET("/ping", func(c *gin.Context) {
