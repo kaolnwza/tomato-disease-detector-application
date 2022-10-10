b9d80dcbccaa87b5a7c7395db115ce3da51d72d4
PwrFr <farrocker@Outlook.co.th>
2022-10-11 00:10:30 +0700

    feat: :sparkles: Camera
    add Camera

--- web/src/screens/camera.js
+++ web/src/screens/camera.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Linking, SafeAreaView, StyleSheet} from 'react-native';
 import {
   Divider,
   Icon,
@@ -10,10 +10,33 @@ import {
   TopNavigationAction,
 } from '@ui-kitten/components';
 
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
+
 export const CameraScreen = ({navigation}) => {
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  React.useEffect(() => {
+    reqCameraPermission();
+  });
+
+  const reqCameraPermission = React.useCallback(async () => {
+    const permission = await Camera.requestCameraPermission();
+
+    if (permission === 'denied') {
+      await Linking.openSettings();
+    }
+  }, []);
+  if (device == null) return null;
   return (
-    <Layout style={styles.container}>
-      <Button onPress={() => {}}>Open Camera</Button>
+    <Layout style={{flex: 1}}>
+      <Camera
+        style={{flex: 1}}
+        device={device}
+        isActive={true}
+        enableZoomGesture
+      />
+      {/* <Button onPress={() => {}}>Open Camera</Button> */}
     </Layout>
   );
 };
