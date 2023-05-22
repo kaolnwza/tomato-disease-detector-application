038b650e03359b2561818070cfc7caadde94a581
kaolnwza <sinlapawit.kao@gmail.com>
2023-05-23 00:33:35 +0700

    Fix torch tranform resize

--- model-server/fastapisrv/main.py
+++ model-server/fastapisrv/main.py
@@ -39,7 +39,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
 transformer = transforms.Compose([
-    transforms.Resize((224, 224)),
+    # transforms.Resize((224, 224)),
     transforms.ToTensor(),
     # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
